71e7fa087655a771749496c3ab962108e3c5a42d
Jason Ye <jasonye4273@gmail.com>
2024-09-09 16:08:59 -0400

    bugfix

--- mythicscraper.py
+++ mythicscraper.py
@@ -9,7 +9,7 @@ async def mythicscraper(client, setcode: str):
   print(f"SCRAPING SET {setcode}")
 
   cur.execute(f"SELECT (channel,altchannel,role,altrole) FROM scraperinfo WHERE setcode='{setcode}'")
-  data = cur.fetchone()
+  data = cur.fetchone()[0]
   if not data:
     return
 
