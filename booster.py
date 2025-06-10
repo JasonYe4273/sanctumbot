6f2b2358f0b44257de7e41b713da2d9db9d60593
Jason Ye <jasonye4273@gmail.com>
2025-06-09 23:49:47 -0400

    log

--- booster.py
+++ booster.py
@@ -19,6 +19,7 @@ def fetch_sealed_data():
 
     resp = requests.get("https://raw.githubusercontent.com/taw/magic-sealed-data/refs/heads/master/sealed_basic_data.json")
     for s in resp.json():
+      print(f"Fetching sealed data for set ${s}")
       if s["code"][-5:] == "draft" or s["code"][-4:] == "play":
         SEALED_DATA[s["setcode"].upper()] = s
 
