b28fe7f2744dce68acc173bfb57bfcc1f31ca8a1
Jason Ye <jasonye4273@gmail.com>
2025-06-10 00:06:21 -0400

    log

--- booster.py
+++ booster.py
@@ -19,7 +19,6 @@ def fetch_sealed_data():
 
     resp = requests.get("https://raw.githubusercontent.com/taw/magic-sealed-data/refs/heads/master/sealed_basic_data.json")
     for s in resp.json():
-      print(f"Fetching sealed data for set ${s['code']}")
       if s["code"][-5:] == "draft" or s["code"][-4:] == "play":
         SEALED_DATA[s["set_code"].upper()] = s
 
@@ -98,6 +97,7 @@ async def generate_pack(interaction: Optional[discord.Interaction], setcode: str
       set_cards = SET_CACHE[set_]
     else:
       try:
+        print(set_)
         resp = requests.get(f"https://mtgjson.com/api/v5/{set_}.json")
         print(f"Fetched {set_} JSON!")
         for c in resp.json()["data"]["cards"]:
