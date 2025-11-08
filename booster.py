5fa574cb33556fc1b289f3170db4463c1fa37dd9
Jason Ye <jasonye4273@gmail.com>
2025-11-08 00:17:16 -0500

    log

--- booster.py
+++ booster.py
@@ -112,6 +112,7 @@ async def generate_pack(interaction: Optional[discord.Interaction], setcode: str
     if not cn.isdigit() and cn[:-1].isdigit():
       cn = cn[:-1]
     if cn not in set_cards:
+      print(set_ + " " + cn)
       if set_ == "tla" and cn == "115":
         pack_names.append("Pirate Peddlers")
       elif set_ == "tla" and cn == "138":
