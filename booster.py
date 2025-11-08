d427ae32b2da687b908a01654ffd1b51a57a2576
Jason Ye <jasonye4273@gmail.com>
2025-11-08 00:12:52 -0500

    fix foils

--- booster.py
+++ booster.py
@@ -112,11 +112,11 @@ async def generate_pack(interaction: Optional[discord.Interaction], setcode: str
     if not cn.isdigit() and cn[:-1].isdigit():
       cn = cn[:-1]
     if cn not in set_cards:
-      if set_cn == "tla:115":
+      if set_ == "tla" and cn == "115":
         pack_names.append("Pirate Peddlers")
-      elif set_cn == "tla:138":
+      elif set_ == "tla" and cn == "138":
         pack_names.append("Firebending Lesson")
-      elif set_cn == "tla:250":
+      elif set_ == "tla" and cn == "250":
         pack_names.append("Wandering Musicians")
       else:
         pack_names.append(pack[i])
