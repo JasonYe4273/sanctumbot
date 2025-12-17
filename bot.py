399e54f2b0780ad95ab352ee4d992e4cc5d4cb3e
Jason Ye <jasonye4273@gmail.com>
2025-12-17 04:44:15 -0500

    check if on messge is working

--- bot.py
+++ bot.py
@@ -40,6 +40,8 @@ async def on_message(message: Message) -> None:
     if message.author == client.user:
         return
 
+    print(message.content)
+
     # Handle data queries of the form '{{query | options}}'
     if (DATA_QUERY_L in message.content) and (DATA_QUERY_R in message.content):
         await handle_card_request_v2(message.content, message.channel)
