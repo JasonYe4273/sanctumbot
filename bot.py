248b28e156dfca33be1b87f703431a0a6a9f8740
Jason Ye <jasonye4273@gmail.com>
2026-02-01 12:03:25 -0500

    fix

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord import app_commands, Message
 from discord.ext import tasks
 
 from database import con, cur, _get_one_db, _get_all_db, _set_db
-from util import client, tree, send_error, TOKEN, TEST, SANCTUM, PT
+from util import client, tree, send_error, TOKEN, TEST, DRAFT, SANCTUM, PT
 
 import misc
 import tournaments
@@ -86,6 +86,7 @@ async def refresh_data() -> None:
 @client.event
 async def on_ready():
     await tree.sync(guild=TEST)
+    await tree.sync(guild=DRAFT)
     await tree.sync(guild=SANCTUM)
     await tree.sync(guild=PT)
 
