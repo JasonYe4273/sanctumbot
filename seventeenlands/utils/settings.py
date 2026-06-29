ae7309965cf4135ffc67b5490a692ae99f2ce97d
JasonILTG <jasonye4273@gmail.com>
2026-06-28 23:09:15 -0400

    update 17l sets

--- seventeenlands/utils/settings.py
+++ seventeenlands/utils/settings.py
@@ -1,5 +1,5 @@
-UPDATING_SETS: list[str] = ['SOS']
-OLD_SETS: list[str] = ['TLA', 'ECL']
+UPDATING_SETS: list[str] = ['MSH']
+OLD_SETS: list[str] = ['SOS', 'ECL']
 SETS: list[str] = UPDATING_SETS + OLD_SETS
 START_DATE: str = '2025-01-01'
 DEFAULT_FORMAT: str = 'PremierDraft'
