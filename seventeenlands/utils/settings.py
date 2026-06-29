4e836a1f314462b49540b178058da51fcce5ad77
JasonILTG <jasonye4273@gmail.com>
2026-06-29 00:09:42 -0400

    dmu

--- seventeenlands/utils/settings.py
+++ seventeenlands/utils/settings.py
@@ -1,5 +1,5 @@
 UPDATING_SETS: list[str] = ['MSH']
-OLD_SETS: list[str] = ['SOS', 'ECL']
+OLD_SETS: list[str] = ['SOS', 'ECL', 'DMU']
 SETS: list[str] = UPDATING_SETS + OLD_SETS
 START_DATE: str = '2025-01-01'
 DEFAULT_FORMAT: str = 'PremierDraft'
