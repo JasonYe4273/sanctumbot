b15b5abfb80914be4d5ab96f18f225723566a143
JasonILTG <jasonye4273@gmail.com>
2026-07-04 02:46:33 -0400

    diagnose

--- bot.py
+++ bot.py
@@ -42,6 +42,8 @@ async def on_message(message: Message) -> None:
     if message.author == client.user:
         return
 
+    print(message.guild.id)
+    print(type(message.guild.id))
     if message.guild.id in MAGIC:
         # Handle data queries of the form '{{query | options}}'
         if (DATA_QUERY_L in message.content) and (DATA_QUERY_R in message.content):
